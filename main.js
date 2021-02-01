1efcc34643b60cfbb604a36ba960d77e5ff2e17a
judy-njeru <njerujudy87@ymail.com>
2021-02-01 13:55:41 +0100

    dynamic div data on DOM load commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const documentFragment = document.createDocumentFragment();
+const numbersDiv = document.querySelector(".numbers");
+
+/* ----------------- Colors -------------------- */
+
+const colors = ['#2B8EAD', '#6F98A8', '#2F454E', '#BFBFBF'] //blue, lightBlue, navyBlue, lightGrey
+
+/* ----------------- Data for the Div Elements with Numbers -------------------- */
+
+const numbersData = [
+    {number: 1, bgColor: colors[1]},
+    {number: 2, bgColor: colors[0]},
+    {number: 3, bgColor: colors[2]},
+    {number: 4, bgColor: colors[0]},
+    {number: 5, bgColor: colors[2]},
+    {number: 6, bgColor: colors[3]},
+    {number: 7, bgColor: colors[3]},
+    {number: 8, bgColor: colors[1]},
+    {number: 9, bgColor: colors[2]},
+]
+
+/* ----------------- Load Div Elements with the Numbers Data on DOM load -------------------- */
+
+document.addEventListener('DOMContentLoaded', () => {
+    for ( let index = 0; index < numbersData.length; index++ ) {
+        let divElement = document.createElement('div');
+        let numberDivElement = document.createElement("div");   
+        let bgColorDivElement = document.createElement("div");  
+
+        numberDivElement.innerHTML = numbersData[index].number;                   
+        divElement.className = "number";
+        divElement.dataset.id = numbersData[index].number;
+        bgColorDivElement.className  = "bg-color";  
+        bgColorDivElement.style.backgroundColor   = numbersData[index].bgColor;;   
+        
+        divElement.appendChild(numberDivElement);                             
+        divElement.appendChild(bgColorDivElement);                              
+        numbersDiv.appendChild(divElement);                          
+    }
+});
